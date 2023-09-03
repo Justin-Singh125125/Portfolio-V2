ce6a69ae0649599516cd5f207846fab4d20591fd
Justin Singh <jsingh@rcglogistics.com>
2023-09-03 13:05:58 -0700

    took off bg color

--- src/app/components/Hero/components/IntroductionDetails/components/IntroductionLink/IntroductionLink.tsx
+++ src/app/components/Hero/components/IntroductionDetails/components/IntroductionLink/IntroductionLink.tsx
@@ -4,7 +4,7 @@ export const IntroductionLink = ({ href, icon }: IntroductionLinkProps) => {
   return (
     <a
       href={href}
-      className="inline-block p-2 [&>svg]:hover:stroke-green [&>svg]:transition-all [&>svg]:duration-1000"
+      className="inline-block p-2 [&>svg]:hover:stroke-green [&>svg]:transition-all [&>svg]:duration-300"
     >
       {icon}
     </a>
