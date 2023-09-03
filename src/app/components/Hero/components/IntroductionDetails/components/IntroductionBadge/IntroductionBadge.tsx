9ad186c28ab2a53b297d2ab41413ebc8334a208f
Justin Singh <jsingh@rcglogistics.com>
2023-09-03 13:12:26 -0700

    fixed badge occuply full width

--- src/app/components/Hero/components/IntroductionDetails/components/IntroductionBadge/IntroductionBadge.tsx
+++ src/app/components/Hero/components/IntroductionDetails/components/IntroductionBadge/IntroductionBadge.tsx
@@ -2,7 +2,7 @@ import { INTRODUCTION_STRING } from './IntroductionBadge.constants';
 
 export const IntroductionBadge = () => {
   return (
-    <div className="p-3 bg-green rounded-2xl rounded-bl-none">
+    <div className="inline-block p-3 bg-green rounded-2xl rounded-bl-none">
       <h5 className="text-white font-light">{INTRODUCTION_STRING}</h5>
     </div>
   );
