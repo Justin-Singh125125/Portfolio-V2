a35324d9412310528fe057cd2894eda197916122
Justin Singh <jsingh@rcglogistics.com>
2023-09-04 13:26:05 -0700

    made hero image a bit bigger

--- src/app/components/Hero/components/HeroImage/HeroImage.tsx
+++ src/app/components/Hero/components/HeroImage/HeroImage.tsx
@@ -3,11 +3,11 @@ import Justin from './images/justin.jpeg';
 
 export const HeroImage = () => {
   return (
-    <div className="w-[19rem] h-[19rem] rounded-full bg-gray-light flex items-center justify-center">
-      <div className="w-[17rem] h-[17rem] rounded-full bg-gray flex items-center justify-center">
+    <div className="w-[24rem] h-[24rem] rounded-full bg-gray-light flex items-center justify-center">
+      <div className="w-[22rem] h-[22rem] rounded-full bg-gray flex items-center justify-center">
         <Image
           priority
-          className="w-[15rem] h-[15rem] rounded-full object-cover"
+          className="w-[20rem] h-[20rem] rounded-full object-cover"
           src={Justin}
           alt="Picture of Justin Singh"
         />
