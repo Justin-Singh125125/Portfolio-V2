fcc9b00ba3e3509f66c9806086ffe80efdb8cbf3
Justin Singh <jsingh@rcglogistics.com>
2023-09-04 12:26:41 -0700

    created base HeroImage component

--- src/app/components/Hero/Hero.tsx
+++ src/app/components/Hero/Hero.tsx
@@ -1,9 +1,10 @@
-import { HeroDetails } from './components';
+import { HeroDetails, HeroImage } from './components';
 
 export const Hero = () => {
   return (
     <section className="h-screen flex items-center justify-center">
       <HeroDetails />
+      <HeroImage />
     </section>
   );
 };
---
+++ src/app/components/Hero/components/HeroImage/HeroImage.tsx
@@ -0,0 +1,3 @@
+export const HeroImage = () => {
+  return <div>HeroImage</div>;
+};
