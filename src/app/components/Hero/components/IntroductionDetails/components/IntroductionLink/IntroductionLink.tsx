09a67550bb48b6c310586789bb4b5190aae92d36
Justin Singh <jsingh@rcglogistics.com>
2023-09-04 07:44:31 -0700

    okay this time i did fix it. I was applying hover effects onto the svg itself rather than the parent. Tailwind arbitary values was a little tricky to understand

--- src/app/components/Hero/components/IntroductionDetails/components/IntroductionLink/IntroductionLink.tsx
+++ src/app/components/Hero/components/IntroductionDetails/components/IntroductionLink/IntroductionLink.tsx
@@ -2,10 +2,7 @@ import { IntroductionLinkProps } from './IntroductionLink.types';
 
 export const IntroductionLink = ({ href, icon }: IntroductionLinkProps) => {
   return (
-    <a
-      href={href}
-      className="inline-block p-2 hover:[&>svg]:fill-green [&>svg]:transition-fill [&>svg]:duration-200"
-    >
+    <a href={href} className="inline-block p-2 [&:hover>svg]:fill-green [&>svg]:transition-fill [&>svg]:duration-300">
       {icon}
     </a>
   );
