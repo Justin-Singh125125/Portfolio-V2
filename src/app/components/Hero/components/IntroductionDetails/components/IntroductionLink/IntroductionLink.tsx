eeccf59d7db8141e15070812e56d1f4332b2f35b
Justin Singh <jsingh@rcglogistics.com>
2023-09-04 07:46:52 -0700

    added expected styles

--- src/app/components/Hero/components/IntroductionDetails/components/IntroductionLink/IntroductionLink.tsx
+++ src/app/components/Hero/components/IntroductionDetails/components/IntroductionLink/IntroductionLink.tsx
@@ -2,7 +2,10 @@ import { IntroductionLinkProps } from './IntroductionLink.types';
 
 export const IntroductionLink = ({ href, icon }: IntroductionLinkProps) => {
   return (
-    <a href={href} className="inline-block p-2 [&:hover>svg]:fill-green [&>svg]:transition-fill [&>svg]:duration-300">
+    <a
+      href={href}
+      className="inline-block p-2 [&:hover>svg]:fill-green [&>svg]:transition-fill [&>svg]:duration-300 hover:bg-black transition-bg duration-300 rounded-md"
+    >
       {icon}
     </a>
   );
