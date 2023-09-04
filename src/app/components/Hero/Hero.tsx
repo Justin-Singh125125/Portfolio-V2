424b0fef39aa4c7eeff20957c13e71ac0fa63742
Justin Singh <jsingh@rcglogistics.com>
2023-09-04 13:26:13 -0700

    added space between the details and the image

--- src/app/components/Hero/Hero.tsx
+++ src/app/components/Hero/Hero.tsx
@@ -2,7 +2,7 @@ import { HeroDetails, HeroImage } from './components';
 
 export const Hero = () => {
   return (
-    <section className="h-screen flex items-center justify-center">
+    <section className="h-screen flex items-center justify-center  space-x-72">
       <HeroDetails />
       <HeroImage />
     </section>
